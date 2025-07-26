7e9542dbce66b29e4059fd7cf05ee036edb05452
thmastin <thmastin@gmail.com>
2025-07-26 17:52:06 -0400

    Add new endpoints

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
 
+var apiCfg apiConfig
+
 func main() {
+
 	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /api/healthz", handlerHealthz)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
 
 	var s http.Server
 	s.Handler = mux
@@ -13,4 +22,35 @@ func main() {
 
 	s.ListenAndServe()
 
+}
+
+func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (apiCfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+func (apiCfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	str := strconv.Itoa(int(apiCfg.fileserverHits.Load()))
+	w.Write([]byte("Hits: " + str))
+}
+
+func (apiCfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	apiCfg.fileserverHits.Store(0)
 }
